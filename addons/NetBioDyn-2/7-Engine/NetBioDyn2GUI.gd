55509a69adfcdf3ca8c3b182b9cf9535cab148e3
Pascal <pascal.ballet@gmail.com>
2022-06-20 00:46:11 +0200

    Adding agents is now possible.

--- addons/NetBioDyn-2/7-Engine/NetBioDyn2GUI.gd
+++ addons/NetBioDyn-2/7-Engine/NetBioDyn2GUI.gd
@@ -1,35 +1,40 @@
 extends Node
 
-
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
 
 var _pm
+var treeAgents
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	addAgent()
+	# Tree of entities
+	treeAgents = find_node("TreeAgents", true, true)
+	treeAgents.set_hide_root(false)
+	addAgent("--------------------")
+	#print("trouvé", treeAgents)
 	_pm = $PopupMenu
 	
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
 
-func addAgent() -> void:
-	var treeAgents = find_node("TreeAgents", true, true)
-	print("trouvé", treeAgents)
-	var agt:TreeItem = treeAgents.create_item()
-	agt.set_text(0, "Agents")
-	#treeAgents.set_hide_root(true)
-	var child1 = treeAgents.create_item(agt)
-	child1.set_text(0, "Child1")
-		#var child2 = treeAgents.create_item(agt)
+func addAgent(var name) -> void:	
+	# Selected node
+	var parent:TreeItem = treeAgents.get_selected()
+	
+	# Create new Agent
+	var agt:TreeItem = treeAgents.create_item(parent)
+	agt.set_text(0, name)
+	#
+	#var child1 = treeAgents.create_item(agt)
+	#child1.set_text(0, "Child1")
+	#var child2 = treeAgents.create_item(agt)
 	#treeAgents.add_item("Agent 1", null, true)
 	#treeAgents.add_item("Agent 2", null, true)
-	var subchild1 = treeAgents.create_item(child1)
-	subchild1.set_text(0, "Subchild1")
+	#var subchild1 = treeAgents.create_item(child1)
+	#subchild1.set_text(0, "Subchild1")
 
 func addTaxon() -> void:
 	pass
@@ -41,7 +46,10 @@ func _on_ToolPlusAgent_pressed() -> void:
 
 func _on_PopupMenu_index_pressed(index: int) -> void:
 	if index == 0:
-		addAgent()
+		addAgent("Agent")
 	if index == 1:
 		addTaxon()
 	pass # Replace with function body.
+
+func _on_TreeAgents_item_selected() -> void:
+	print("clic") #item, column, id)
