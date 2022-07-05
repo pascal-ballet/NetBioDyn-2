af11f3284da542d3e9c60858dee7972e4c1ebd44
pascal-ballet <pascal.ballet@gmail.com>
2022-07-05 13:37:58 +0200

    Agent Node in progress

--- addons/NetBioDyn-2/7-Engine/agent.gd
+++ addons/NetBioDyn-2/7-Engine/agent.gd
@@ -3,8 +3,7 @@ extends Node
 class_name NBD_Agent
 
 # Declare member variables here.
-export(String) var agent_name
-
+export(Color)  var _color = Color(0.0 , 0.2 , 1.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
